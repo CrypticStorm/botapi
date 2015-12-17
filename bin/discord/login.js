c69f19bb2391055494a2e57507b9ff6b67ad6a99
CrypticStorm <crypticstorm.gamersconference@gmail.com>
2015-12-17 15:52:21 -0500

    Add token login support

--- bin/discord/login.js
+++ bin/discord/login.js
@@ -1,7 +1,15 @@
 module.exports = function() {
     var lines = require("fs").readFileSync("cfg/login.txt", "utf-8").split(require("os").EOL);
-    return {
-        email: lines[0],
-        password: lines[1]
+    if (lines.length == 0) {
+        throw 'no login';
+    } else if (lines.length == 1) {
+        return {
+            token: lines[0]
+        }
+    } else {
+        return {
+            email: lines[0],
+            password: lines[1]
+        }
     }
 }
