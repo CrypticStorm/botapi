2b42b97a182a571e4865cee8511d5f24b67ef104
CrypticStorm <crypticstorm.gamersconference@gmail.com>
2015-12-17 20:37:48 -0500

    Move file io and post listener to load, channel remapping to enable.

--- bin/core/plugins/github.js
+++ bin/core/plugins/github.js
@@ -6,38 +6,38 @@ const os = require('os');
 const Commands = {
     name: 'Core-Github',
     load: function(manager) {
-        var github_json = JSON.parse(fs.readFileSync('cfg/github.json', 'utf-8'));
-        manager.bot.config.github_json = github_json;
-        var channels = github_json.channels;
-        var secrets = github_json.secrets;
-        console.log(channels);
-        console.log(secrets);
-        if (channels.length > 0 && secrets.length > 0) {
-            manager.bot.app.post('/github/bot', function (req, res) {
-                if (req.headers['x-github-event'] == 'push') {
-                    var branch = req.body.ref.substring(req.body.ref.lastIndexOf('/') + 1);
-                    var head_url = req.body.head_commit.url;
-                    var commits = req.body.commits;
-                    var message = 'New commit' + (commits.length > 1 ? 's' : '') + ' to `' + branch + '`:\n\n'
-                    for (var i = 0; i < commits.length; i++) {
-                        message += '`' + commits[i].id.substring(0, 8) + '`: ' + commits[i].message + ' (' + commits[i].author.name + ')' + '\n';
-                    }
-                    message += '\n' + head_url + '\n';
-
-                    for (var i = 0; i < channels.length; i++) {
-                        manager.bot.client.Channels.get(channels[i]).sendMessage(message);
-                    }
-                } else {
-                    console.log('Unknown github message:');
-                    console.log(req.headers);
-                    console.log(req.body);
+        manager.bot.config.github_json = JSON.parse(fs.readFileSync('cfg/github.json', 'utf-8'));
+        manager.bot.app.post('/github/bot', function (req, res) {
+            if (req.headers['x-github-event'] == 'push') {
+                console.log(req.headers);
+                var reponame = req.body.repository.name;
+                var branch = req.body.ref.substring(req.body.ref.lastIndexOf('/') + 1);
+                var head_url = req.body.head_commit.url;
+                var commits = req.body.commits;
+                var message = 'New commit' + (commits.length > 1 ? 's' : '') + ' to ' + reponame + ' at branch `' + branch + '`:\n\n'
+                for (var i = 0; i < commits.length; i++) {
+                    message += '`' + commits[i].id.substring(0, 8) + '`: ' + commits[i].message + ' (' + commits[i].author.name + ')' + '\n';
                 }
-                res.send('ok');
-            });
-        }
+                message += '\n' + head_url + '\n';
+
+                for (var i = 0; i < manager.bot.config.github_json.channels.length; i++) {
+                    manager.bot.config.github_json.channels[i].sendMessage(message);
+                }
+            } else {
+                console.log('Unknown github message:');
+                console.log(req.headers);
+                console.log(req.body);
+            }
+            res.send('ok');
+        });
     },
     enable: function(manager) {
-
+        var channel_ids = manager.bot.config.github_json.channels;
+        var channels = [];
+        for (var i = 0; i < channel_ids.length; i++) {
+            channels.push(manager.bot.client.Channels.get(channel_ids[i]));
+        }
+        manager.bot.config.github_json.channels = channels;
     }
 };
 
