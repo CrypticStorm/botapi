5897542a3ce7c036f56b6c1096c994d08cb7e4cd
CrypticStorm <crypticstorm.gamersconference@gmail.com>
2015-12-18 14:47:14 -0500

    Add route remover

--- bin/core/utils.js
+++ bin/core/utils.js
@@ -1,6 +1,13 @@
 var Utils = {
-    escapeRegExp: function(str){
+    escapeRegExp: function(str) {
         return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+    },
+    deleteRoute: function(app, url) {
+        for (var i = app.routes.get.length - 1; i >= 0; i--) {
+            if (app.routes.get[i].path === "/" + url) {
+                app.routes.get.splice(i, 1);
+            }
+        }
     }
 };
 
