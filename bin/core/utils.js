e58641b94ff61f56326a5214b02a4bf7e0a7f3cc
CrypticStorm <crypticstorm.gamersconference@gmail.com>
2015-12-19 21:51:22 -0500

    Remove deleteRoutes in favor of no need for plugin access to app

--- bin/core/utils.js
+++ bin/core/utils.js
@@ -1,16 +1,6 @@
 var Utils = {
     escapeRegExp: function(str) {
         return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
-    },
-    deleteRoute: function(app, url) {
-        if (!url.startsWith('/')) {
-            url = '/' + url;
-        }
-        for (var i = app.routes.get.length - 1; i >= 0; i--) {
-            if (app.routes.get[i].path === url) {
-                app.routes.get.splice(i, 1);
-            }
-        }
     }
 };
 
